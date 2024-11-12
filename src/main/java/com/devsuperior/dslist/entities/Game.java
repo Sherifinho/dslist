839de94fb6efaa6845acca553c30d3772432e27e
Jonathan Cesar <jonathan10ccg@gmail.com>
2024-11-11 22:40:12 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,14 +20,19 @@ public class Game
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game ( Long id, String title, Integer year, String genre, String platforms, Double score,String imgUrl, String shortDescription, String longDescription)
+    public Game ( Long id, String title, Integer year, String genre, String platforms,
+                  Double score,String imgUrl, String shortDescription, String longDescription)
     {
         this.title = title;
         this.year = year;
